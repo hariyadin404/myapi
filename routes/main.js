f5fe799b3db908a9878e14bf662a22002297b3b9
hariyadin <79754073+hariyadin404@users.noreply.github.com>
2021-10-13 21:58:36 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,12 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+router.get('', (req, res) => {
+    res.sendFile(__path + '/views/home.html')
 })
 
-router.get('/buy', (req, res) => {
-    res.sendFile(__path + '/views/buy.html')
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/index.html')
 })
 
 router.get('/sosial', (req, res) => {
